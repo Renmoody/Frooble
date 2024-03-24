1266009894366a133493bc2e8ba723252b26bb63
Jtcrawhorn <Jtcrawhorn@gmail.com>
2024-03-23 22:55:22 -0400

    Pushing the java util files

---
+++ app/src/main/java/com/example/frooble/Email.java
@@ -0,0 +1,62 @@
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Email extends Authenticator {
+
+    private String email;
+    private String message;
+
+    public Email(Contact contact, String message) {
+        this.email = contact.getEmail();
+        this.message = message;
+    }
+
+    public static void send() {
+        String host = "smtp.gmail.com";
+        String port = "587";
+        String username = "fubleproject@gmail.com";
+        String password = "FubleProjectEmail";
+        Properties props = new Properties();
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        // Import Session from javax.mail package and use it
+        Session session = Session.getInstance(props, new Authenticator() {
+            protected javax.mail.PasswordAuthentication getPasswordAuthentication() {
+                return new javax.mail.PasswordAuthentication(username, password);
+            }
+        });
+        try {
+            // Create a default MimeMessage object
+            Message mimeMessage = new MimeMessage(session);
+
+            // Set From: header field of the header
+            mimeMessage.setFrom(new InternetAddress(username));
+
+            // Set To: header field of the header
+            mimeMessage.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+
+            // Set Subject: header field
+            mimeMessage.setSubject("Fruble Detected an Emergency!");
+
+            // Now set the actual message
+            mimeMessage.setText(message);
+
+            // Send message
+            Transport.send(mimeMessage);
+
+            System.out.println("Email sent successfully");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/frooble/GradelBuildcopy.java
@@ -0,0 +1,6 @@
+// https://mvnrepository.com/artifact/javax.activation/activation
+    implementation("javax.activation:activation:1.1.1")
+    // https://mvnrepository.com/artifact/javax.mail/mail
+    implementation("javax.mail:mail:1.4.7")
+    // https://mvnrepository.com/artifact/javax.mail/mail
+    implementation("javax.mail:mail:1.4")
