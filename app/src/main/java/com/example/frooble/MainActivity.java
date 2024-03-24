275d8c453f48100b2de76bff947461e5bf55fda7
James Owen <owenjd@dukes.jmu.edu>
2024-03-24 02:49:27 -0400

    App layout with contact 1 button

--- app/src/main/java/com/example/frooble/MainActivity.java
+++ app/src/main/java/com/example/frooble/MainActivity.java
@@ -7,9 +7,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import com.example.frooble.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextInputEditText email1Id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +26,19 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button contact1 = findViewById(R.id.contact1activity);
+        email1Id = findViewById(R.id.email1Id);
+        // textInputEditText = findViewById(R.id.email1Id);
+        contact1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Contact1Activity.class);
+                startActivity(intent);
+                String userInput = email1Id.getText().toString();
+                Toast.makeText(MainActivity.this, "User input: " + userInput, Toast.LENGTH_SHORT).show();
+            }
         });
     }
 }
